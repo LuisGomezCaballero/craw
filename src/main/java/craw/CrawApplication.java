86238948978f0e8c339dd1d1512e569b1905c0a1
Luis Gómez Caballero <lgomezc@vectoritcgroup.com>
2018-04-20 13:15:12 +0200

    Activar despliegue standalone

--- src/main/java/craw/CrawApplication.java
+++ src/main/java/craw/CrawApplication.java
@@ -2,11 +2,18 @@ package craw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrawApplication {
+public class CrawApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrawApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CrawApplication.class);
 	}
 }
