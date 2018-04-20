4a10551d345e06753b033940c7f866befa3dd2e1
Luis Gómez Caballero <lgomezc@vectoritcgroup.com>
2018-04-20 11:50:46 +0200

    Crear proyecto SpringBoot

---
+++ src/main/java/craw/CrawApplication.java
@@ -0,0 +1,12 @@
+package craw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawApplication.class, args);
+	}
+}
