ed595058eae52a9b68cb38ced0a51e79194dcead
egonzalezc <egonzalezc@L0000451.mshome.net>
2018-04-20 15:01:54 +0200

    Vista y controlador

--- src/main/java/craw/jdbc/controller/JdbcController.java
+++ src/main/java/craw/jdbc/controller/JdbcController.java
@@ -1,5 +1,40 @@
 package craw.jdbc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import craw.jdbc.service.JdbcService;
+
+
+@Controller
+@RequestMapping("/jdbc")
 public class JdbcController {
 
+	@Autowired
+	private JdbcService jdbcService;
+	
+    
+	    @RequestMapping("/form")
+	    public String showUserForm(Model model){
+	    	System.out.println("Mostrando formulario");
+	        return "index";
+	    }
+	    
+		@RequestMapping(method = RequestMethod.POST, value = "/comprobar")
+	    public ModelAndView comprobar() {
+			System.out.println("Entrando en el método comprobar....");
+			//Boolean hayConexion = jdbcService.comprobarTabla();
+			Boolean hayConexion = Boolean.TRUE;
+	        ModelAndView model = new ModelAndView();
+	        model.addObject("hayConexion", hayConexion);
+	        model.setViewName("index");
+	        return model;
+	    }
 }
--- src/main/java/craw/jdbc/dao/JdbcDaoImpl.java
+++ src/main/java/craw/jdbc/dao/JdbcDaoImpl.java
@@ -1,23 +1,23 @@
 package craw.jdbc.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class JdbcDaoImpl implements JdbcDao {
 
-	@Autowired
-	JdbcTemplate jdbcTemplate;
+//	@Autowired
+	//JdbcTemplate jdbcTemplate;
 
 	@Override
 	public Boolean comprobarTabla() {
 
 		Boolean exito = null;
 		try {
-			String string = jdbcTemplate.queryForObject("SELECT 1 FROM DUAL", String.class);
+			//String string = jdbcTemplate.queryForObject("SELECT 1 FROM DUAL", String.class);
 			exito = Boolean.TRUE;
-			System.out.println("He encontrado el siguiente valor: " + string);
+			//System.out.println("He encontrado el siguiente valor: " + string);
 		} catch (Exception e) {
 			exito = Boolean.FALSE;
 			System.out.println("La excepción es: " + e);
